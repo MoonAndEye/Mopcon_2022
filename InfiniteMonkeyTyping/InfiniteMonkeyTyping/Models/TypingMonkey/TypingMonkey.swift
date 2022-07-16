b92580f31cdb44010623ba9ca2950f2bf75f8b1b
MarvinLin <atimis19@gmail.com>
2022-07-16 23:09:35 +0800

    feat(add): create files

---
+++ InfiniteMonkeyTyping/InfiniteMonkeyTyping/Models/TypingMonkey/TypingMonkey.swift
@@ -0,0 +1,18 @@
+//
+//  TypingMonkey.swift
+//  InfiniteMonkeyTyping
+//
+//  Created by cm0679 on 2022/7/16.
+//
+
+import Foundation
+
+/// 打字的猴子
+struct TypingMonkey {
+    
+    // TODO: 之後可以再調，所以要開出 set function
+    // 產出一個字的時間
+    private let typingInterval: TimeInterval = 0.01
+    
+    
+}
