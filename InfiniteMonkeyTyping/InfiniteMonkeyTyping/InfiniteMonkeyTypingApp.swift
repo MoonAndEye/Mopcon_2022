7db24fa05da48cb3e147fb9064f5d3f6e4a49f2a
MarvinLin <atimis19@gmail.com>
2022-07-16 16:25:26 +0800

    add: file, InfiniteMonkeyTypingContentView

--- InfiniteMonkeyTyping/InfiniteMonkeyTyping/InfiniteMonkeyTypingApp.swift
+++ InfiniteMonkeyTyping/InfiniteMonkeyTyping/InfiniteMonkeyTypingApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct InfiniteMonkeyTypingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            InfiniteMonkeyTypingContentView()
         }
     }
 }
---
+++ InfiniteMonkeyTyping/InfiniteMonkeyTyping/Views/InfiniteMonkeyTypingContentView.swift
@@ -0,0 +1,30 @@
+//
+//  InfiniteMonkeyTypingContentView.swift
+//  InfiniteMonkeyTyping
+//
+//  Created by cm0679 on 2022/7/16.
+//
+
+import SwiftUI
+
+struct InfiniteMonkeyTypingContentView: View {
+    
+    
+    
+    var body: some View {
+        
+        VStack {
+            Text("無限猴子打字機")
+                .font(.largeTitle)
+                .padding(.top, 20)
+            Spacer()
+        }
+        
+    }
+}
+
+struct InfiniteMonkeyTypingContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfiniteMonkeyTypingContentView()
+    }
+}
