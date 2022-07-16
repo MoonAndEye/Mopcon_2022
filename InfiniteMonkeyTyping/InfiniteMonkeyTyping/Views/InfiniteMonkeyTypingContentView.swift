0d0f1464e8b08166368617aa96cc40930170d499
MarvinLin <atimis19@gmail.com>
2022-07-16 23:00:15 +0800

    feat(add): -加上 Monkey 設定 -加上 UIKit 的 TextView

--- InfiniteMonkeyTyping/InfiniteMonkeyTyping/Views/InfiniteMonkeyTypingContentView.swift
+++ InfiniteMonkeyTyping/InfiniteMonkeyTyping/Views/InfiniteMonkeyTypingContentView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct InfiniteMonkeyTypingContentView: View {
     
+    @State private var targetText = ""
     
+    @State private var monkeyTyperCount = 1
+    
+    @State private var logText = ""
+    
+    @State private var textStyle = UIFont.TextStyle.body
+    
+    private var targetHint: String {
+        if targetText.isEmpty {
+            return "目前沒目標，請輸入目標文字在輸入框"
+        }
+        return "你的目標為: \(targetText)"
+    }
     
     var body: some View {
         
@@ -17,9 +30,77 @@ struct InfiniteMonkeyTypingContentView: View {
             Text("無限猴子打字機")
                 .font(.largeTitle)
                 .padding(.top, 20)
+            
+            Text(targetHint)
+                .lineLimit(1)
+                .padding()
+            
+            TextField("請輸入目標", text: $targetText)
+                .padding()
+                .textFieldStyle(.roundedBorder)
+            
+            createMonkeyTyperStepper()
+            
+            Button("叫猴子開始打字囉") {
+                askMonkeysTyping()
+            }
+            .buttonStyle(.bordered)
+            .padding()
+            
+            HStack {
+                Spacer()
+                Text("猴子的打字紀錄")
+                Button {
+                    clearMonkeyTypeLog()
+                } label: {
+                    Text("清除打字紀錄")
+                }
+                .padding(.leading, 20)
+                .buttonStyle(.bordered)
+                Spacer()
+            }
+            
+            TextView(text: $logText, textStyle: $textStyle)
+                .border(Color.blue)
+                .padding()
             Spacer()
         }
-        
+    }
+    
+    @ViewBuilder
+    private func createMonkeyTyperStepper() -> some View {
+        HStack {
+            Stepper("猴子數: \(monkeyTyperCount)") {
+                stepperIncrease()
+            } onDecrement: {
+                stepperDecrease()
+            }
+        }
+        .padding()
+    }
+}
+// MARK: - Logic part
+extension InfiniteMonkeyTypingContentView {
+    private func stepperIncrease() {
+        monkeyTyperCount += 1
+    }
+    
+    private func stepperDecrease() {
+        if monkeyTyperCount > 1 {
+            monkeyTyperCount -= 1
+        }
+    }
+    
+    private func askMonkeysTyping() {
+        // TODO: 叫現在所有的猴子開始打字
+    }
+    
+    private func stopMonkeysTyping() {
+        // TODO: 讓猴子停手
+    }
+    
+    private func clearMonkeyTypeLog() {
+        logText = ""
     }
 }
 
