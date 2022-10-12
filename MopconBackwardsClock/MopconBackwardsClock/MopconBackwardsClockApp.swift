082a29a9e2301af1cf08991abb1fb638c851423d
MarvinLin <atimis19@gmail.com>
2022-10-12 13:21:38 +0800

    feat: 針與角度完成，接下來做 AngleUtility

--- MopconBackwardsClock/MopconBackwardsClock/MopconBackwardsClockApp.swift
+++ MopconBackwardsClock/MopconBackwardsClock/MopconBackwardsClockApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct MopconBackwardsClockApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ClockContainerView()
     }
+  }
 }
--- MopconBackwardsClock/MopconBackwardsClock/Views/ClockContainerView.swift
+++ MopconBackwardsClock/MopconBackwardsClock/Views/ClockContainerView.swift
@@ -6,8 +6,23 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ClockContainerView: View {
+  
+  // mopcon112: State 角度
+  // next: 做出 timer 連上 view
+  @State var hourAngle: Angle = .degrees(60)
+  
+  @State var minuteAngle: Angle = .degrees(300)
+  
+  @State var secondAngle: Angle = .degrees(180)
+  
+  // mopcon113: timer publish
+  // next: view onRecieve timestamp
+  private var timer = Timer.publish(every: 0.1,  on: .main, in: .common)
+    .autoconnect()
+  
   var body: some View {
     
     // mopcon110: 畫出錶盤，加上數字
@@ -21,16 +36,21 @@ struct ClockContainerView: View {
       // next: rotation 改角度後，加上 state
       HandShape(handLength: .hour)
         .fill(.black)
-        .rotationEffect(.degrees(60))
+        .rotationEffect(hourAngle)
       HandShape(handLength: .minute)
         .fill(.blue)
-        .rotationEffect(.degrees(300))
+        .rotationEffect(minuteAngle)
       HandShape(handLength: .second)
         .fill(.red)
-        .rotationEffect(.degrees(180))
+        .rotationEffect(secondAngle)
     }
     .frame(width: 300, height: 300)
     .padding()
+    // mopcon114: 接收 timer 送過來的訊號
+    // next: 寫 AngleUtility 將 timestamp 轉換成 時/分/秒針 角度
+    .onReceive(timer) { timestamp in
+      print("got time: \(timestamp)")
+    }
   }
 }
 
